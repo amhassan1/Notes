2516ff62d1fddef50ae0b0349c64cf41b38bb921
Abdulaziz Hassan <abdu2812.h@gmail.com>
2022-08-18 02:59:49 +0200

    mod: color picker

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,8 +1,8 @@
 <template>
-    <v-radio-group v-model="colorValue" inline hide-details>
-        <v-radio v-for="color in colors" :value="color" :color="color"
+    <v-radio-group v-model="colorValue" inline hide-details :label="label" class="colorPicker">
+        <v-radio v-for="color in colors" :value="color"
             ><template v-slot:label>
-                <div class="colorPicker" :style="{ backgroundColor: color }"></div>
+                <div class="color" :style="{ backgroundColor: color }"></div>
             </template>
         </v-radio>
     </v-radio-group>
@@ -10,22 +10,25 @@
 
 <script>
     export default {
-        props: { colorValue: String, colors: Array },
+        props: { colorValue: String, colors: Array, label: String },
     };
 </script>
 
 <style scoped>
     .colorPicker {
-        min-width: 45px;
-        min-height: 18px;
+        padding-left: 5px;
+    }
+    .color {
+        width: 25px;
+        height: 18px;
         border-radius: 3px;
         border: 1px solid black;
     }
 
     @media screen and (max-width: 480px) {
-        .colorPicker {
-            min-width: 16px;
-            min-height: 16px;
+        .color {
+            width: 16px;
+            height: 16px;
             border-radius: 3px;
         }
     }
